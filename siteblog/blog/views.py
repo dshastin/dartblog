4641818e04edf6ce874dadc393ddb22eab7f32ed
Dionis <denis.shastin@gmail.com>
2022-08-02 14:00:04 +0300

    menu tamplatetag added

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'blog\index.html')
+
+
+def get_category(request, slug):
+    return render(request, 'blog\category.html')
