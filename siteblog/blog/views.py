012af89ba23ce5977d9c0901f7ad420fc1d9c2ab
Dionis <denis.shastin@gmail.com>
2022-08-20 01:00:16 +0300

    one post done

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 
 class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,13 +16,33 @@ class Home(ListView):
         return context
 
 
-def index(request):
-    return render(request, 'blog\index.html')
+class PostsByCategory(ListView):
+    template_name = 'blog/category.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
 
 
-def get_category(request, slug):
-    return render(request, 'blog\category.html')
+class PostsByTag(ListView):
+    pass
 
+class SinglePost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
-def get_post(request, slug):
-    return render(request, 'blog\post.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
